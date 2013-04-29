41b4d75fa14383fcf6d1b815b16e23de1ce7b481
Ivan Gromakovsky <gie1994@gmail.com>
2013-04-29 18:20:12 +0400

    fdct works fine

--- Task2/task2.asm
+++ Task2/task2.asm
@@ -1,130 +1,196 @@
-%define MATRIX_HEIGHT 1
-%define MATRIX_WIDTH 1
-%define MATRIX_SIZE 4 * MATRIX_HEIGHT * MATRIX_WIDTH
-%define NUMBER_OF_MATRIXES 2
-%define OUTPUT_SIZE NUMBER_OF_MATRIXES * MATRIX_HEIGHT * MATRIX_WIDTH
-extern printf
-extern exit
+%define MATRIX_HEIGHT 8
+%define MATRIX_WIDTH 8
+%define MATRIX_SIZE MATRIX_HEIGHT * MATRIX_WIDTH
 
-global main
+global _fdct
+global _idct
 
-section .rodata
-formatDouble db "%lf ", 0
-formatInt db "%d", 10, 0
-input dd 1.0, 2.1, 2.1, 2.1, 2.1
-two dd 2.
-half dd 0.5
-four dd 4.
-
-
-section .data
-output resd OUTPUT_SIZE
-sqrt2 resd 1
+section .bss
+transformMatrix resd MATRIX_SIZE
+transformMatrixT resd MATRIX_SIZE
+sqrt132 resd 1 ; sqrt132 = sqrt(1/32)
 
 section .text
 
-ret_label:
-    ret
-
-_countSqrt2@0: ; stores sqrt(2.) in sqrt2
-    fld dword[two]    
-    fsqrt
-    fstp dword[sqrt2]
-    ret
-
-_alpha@4: ; arg == 0 ? sqrt(2) / 4 : 0.5
-    push ebp
-    mov ebp, esp
-    mov eax, [esp + 8]
-    test eax, eax
-    jz _alpha_label1
-    fld dword[half]
-    jmp _alpha_label_finish
-  _alpha_label1:
-    fld dword[sqrt2]
-    fdiv dword[four]
-  _alpha_label_finish:
-    leave
-    ret 4
-
-    
-_fdct@12:
+_fdct: ; void _fdct(float * input, float * output, unsigned int n)
     ;frame pointer
     push ebp
     mov ebp, esp
     ;
     push edi
     push esi
+    push ebx
 
-    mov ecx, [esp + 24]
-    mov edi, [esp + 20]
-    mov esi, [esp + 16]
+    call _calculateSqrt132
+    call _calculateTransformMatrix
 
-  _fdct_label_loop:
+    mov ebx, [esp + 28]
+    mov edi, [esp + 24]
+    mov esi, [esp + 20]
+
+  fdct_loop:
     push edi
     push esi
-    call _fdctSingle@8
+    call _fdctSingle
+    add esp, 8
      
-    add edi, MATRIX_SIZE
-    add esi, MATRIX_SIZE
-    dec ecx
-    jnz _fdct_label_loop
+    add edi, MATRIX_SIZE * 4
+    add esi, MATRIX_SIZE * 4
+    dec ebx
+    jnz fdct_loop
 
+    pop ebx
     pop esi
     pop edi
     leave
-    ret 12
+    ret
 
-_fdctSingle@8:  ; doesn't affect ecx
-    ;frame pointer
-    push ebp
-    mov ebp, esp
-    ;
+_fdctSingle: ; void _fdctSingle(float * input, float * output)
     push edi
     push esi
-
-    mov edi, [esp + 20]
-    mov esi, [esp + 16]
-    fld dword[esi]
-    sub esp, 8
-    fstp qword[esp]
-    push formatDouble
-    mov ebx, ecx
-    call printf
-    mov ecx, ebx
-    add esp, 12
-
-    pop esi
-    pop edi
-    leave
-    ret 8
-
-_idct@12:
     ;frame pointer
     push ebp
     mov ebp, esp
     ;
 
+    and esp, -16 ; align stack
+    sub esp, MATRIX_SIZE * 4
 
-    leave
-    ret 12
-
-main:
-    ; frame pointer
-    push ebp
-    mov ebp, esp
-    ;
-
-    call _countSqrt2@0
-
-    push NUMBER_OF_MATRIXES
-    push output
-    push input
-    call _fdct@12
+    mov edi, esp
+    mov esi, [ebp + 16]
 
     xor eax, eax
+  fdctSingle_loop1:
+    xor ecx, ecx
+      fdctSingle_loop2:
+        lea edx, [ecx * MATRIX_WIDTH]
+        movaps xmm0, [esi + edx * 4]
+        add edx, 4
+        movaps xmm1, [esi + edx * 4]
+        lea edx, [eax * MATRIX_WIDTH]
+        mulps xmm0, [transformMatrix + edx * 4] 
+        add edx, 4
+        mulps xmm1, [transformMatrix + edx * 4] 
+        haddps xmm0, xmm1
+        haddps xmm0, xmm0
+        haddps xmm0, xmm0
+
+        movss dword [edi], xmm0
+        add edi, 4
+        inc ecx
+        cmp ecx, MATRIX_WIDTH
+        jne fdctSingle_loop2
+    inc eax
+    cmp eax, MATRIX_HEIGHT
+    jne fdctSingle_loop1
+        
+    mov edi, [ebp + 20]
+    mov esi, transformMatrix
+
+    xor eax, eax
+  fdctSingle_loop3:
+    xor ecx, ecx
+      fdctSingle_loop4:
+        lea edx, [ecx * MATRIX_WIDTH]
+        movaps xmm0, [esi + edx * 4]
+        add edx, 4
+        movaps xmm1, [esi + edx * 4]
+        lea edx, [eax * MATRIX_WIDTH]
+        mulps xmm0, [esp + edx * 4] 
+        add edx, 4
+        mulps xmm1, [esp + edx * 4] 
+        haddps xmm0, xmm1
+        haddps xmm0, xmm0
+        haddps xmm0, xmm0
+
+        lea edx, [eax + MATRIX_WIDTH * ecx]
+        movss dword [edi + edx * 4], xmm0
+        inc ecx
+        cmp ecx, MATRIX_WIDTH
+        jne fdctSingle_loop4
+    inc eax
+    cmp eax, MATRIX_HEIGHT
+    jne fdctSingle_loop3
+
+    leave
+    pop esi
+    pop edi
+    ret
+
+_idct:
+    ;frame pointer
+    push ebp
+    mov ebp, esp
+    ;
+
+
+    leave
+    ret 12
+
+
+_calculateSqrt132: ; stores sqrt(1/32) in sqrt132
+    push dword 0.03125
+    fld dword[esp]    
+    fsqrt
+    fstp dword[sqrt132]
+    add esp, 4
+    ret
+
+_calculateTransformMatrix: ;calculates transformMatrix and transformMatrixT
+    xor eax, eax
+    
+  calculateTransformMatrix_loop1:
+    xor ecx, ecx
+
+      calculateTransformMatrix_loop2:
+        call _calculateTransformMatrixSingle
+        
+        inc ecx
+        cmp ecx, 8
+        jne calculateTransformMatrix_loop2
+
+    inc eax
+    cmp eax, 8
+    jne calculateTransformMatrix_loop1
+
+    ret
+    
+_calculateTransformMatrixSingle: ; void _calculateTransformMatrixSingle(size_t i, size_t j)
+                                ; transformMatrix[i, j] = i == 0 ? 1/8 : 1/sqrt(32) * cos((2 * j + 1) * i * pi / 16)
+                                ; args: eax = i, ecx = j
+    ;frame pointer
+    push ebp
+    mov ebp, esp
+    ;
+
+    test eax, eax
+    jz calculateTransformMatrixSingle_zero
+
+    lea edx, [2 * ecx + 1]
+    fldpi
+    push eax 
+    push edx 
+    fimul dword [esp + 4]
+    fimul dword [esp]
+    add esp, 8
+    push dword 16.0
+    fdiv dword [esp]
+    add esp, 4
+    fcos
+    fmul dword [sqrt132]
+    lea edx, [ecx + eax * MATRIX_WIDTH]
+    fst dword [edx * 4 + transformMatrix]
+    lea edx, [eax + ecx * MATRIX_WIDTH]
+    fstp dword [edx * 4 + transformMatrixT]
+
     leave
     ret
 
-
+  calculateTransformMatrixSingle_zero:
+    mov dword [ecx * 4 + transformMatrix], dword 0.125
+    lea edx, [ecx * MATRIX_WIDTH]
+    mov dword [edx * 4 + transformMatrixT], dword 0.125
+    
+    leave
+    ret
 
